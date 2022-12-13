664ee5b603038dc123fb3adef9d4255e1907f880
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2022-12-13 15:39:17 +0800

    added 5 exceptions to an array

--- MyException/Program.cs
+++ MyException/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyException;
+internal class Program
+{
+   private static void Main(string[] args)
+   {
+      ArgumentException argumentException         = new("The passed argument is incorrect. No I won't tell you how to fix.");
+      DivideByZeroException divideByZeroException = new("You're just testing me aren't you?");
+      FormatException formatException             = new("Ah right, you're supposed to pass a number btw");
+      FileNotFoundException fileNotFoundException = new("Are you sure the file exists? Bc it doesn't");
+      WrongWeekDay wrongWeekDay                   = new("Bad weekday. Try running the code tomorrow. May work, but I don't promise anything");
+      Exception[] exceptions = {argumentException, divideByZeroException, formatException, fileNotFoundException, wrongWeekDay};
+   }
+}
